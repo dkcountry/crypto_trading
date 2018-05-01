66cf5d003c6b2e9f4eec39e2ff535773a9093a7b
David Kang <dkang009@gmail.com>
2018-05-01 00:13:27 -0500

    adds resting exit order

--- order_depth_bot.py
+++ order_depth_bot.py
@@ -3,17 +3,17 @@ from requests.auth import AuthBase
 
 
 class CoinbaseExchangeAuth(AuthBase):
-    def __init__(self, api_key, secret_key, passphrase):
+    def __init__(self, api_key, secret, passphrase):
         self.api_key = api_key
-        self.secret_key = secret_key
+        self.secret_key = secret
         self.passphrase = passphrase
 
     def __call__(self, request):
         timestamp = str(time.time())
-        message = timestamp + request.method + request.path_url + (request.body or '')
+        message = timestamp + request.method + request.path_url + (request.body or b'').decode()
         hmac_key = base64.b64decode(self.secret_key)
-        signature = hmac.new(hmac_key, message, hashlib.sha256)
-        signature_b64 = signature.digest().encode('base64').rstrip('\n')
+        signature = hmac.new(hmac_key, message.encode(), hashlib.sha256)
+        signature_b64 = base64.b64encode(signature.digest()).decode()
 
         request.headers.update({
             'CB-ACCESS-SIGN': signature_b64,
@@ -82,10 +82,32 @@ def execute_trade(sig, size, product):
         'time_in_force': 'IOC'
     }
     trade = requests.post(api_url + 'orders', json=order, auth=auth).json()
-    print("      " + side + " " + str(price) + " " + order['product_id'] + " at " + str(order['price']))
+    print("      " + side + " " + str(size) + " " + order['product_id'] + " at " + str(order['price']))
     print("====================")
     print(trade)
     print("====================")
+    try:
+        return trade
+    except:
+        return None
+
+
+def execute_exit_trade(sig, size, product, entry, gain):
+    if sig > 0:
+        side = 'sell'
+        price = entry + gain
+    if sig < 0:
+        side = 'buy'
+        price = entry - gain
+
+    order = {
+        'size': size,
+        'price': price,
+        'side': side,
+        'product_id': product,
+        'post_only': True
+    }
+    trade = requests.post(api_url + 'orders', json=order, auth=auth).json()
     try:
         return trade
     except:
@@ -109,11 +131,21 @@ def exit_signal(sig, entry, profit, loss):
     return False
 
 
+def stop_loss(sig, entry, loss):
+    r = requests.get(api_url + 'products/' + "LTC-USD" + "/book").json()
+    if sig > 0 and float(r['bids'][0][0]) <= (entry - loss):
+        return True
+    if sig < 0 and float(r['asks'][0][0]) >= (entry + loss):
+        return True
+    return False
+
+
 def order_depth_bot():
     while True:
         traded = False
         filled_quantity = 0.0
-        price = None
+        entry = None
+        exit_trade = None
 
         r = requests.get(api_url + 'products/' + "LTC-USD" + "/book").json()
         signal = order_pressure(ratio, depth)
@@ -126,54 +158,69 @@ def order_depth_bot():
             new_signal = order_pressure(ratio, depth)
 
             while continued_signal(signal, new_signal, ratio) and not traded:
+                print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " signal: " + str(new_signal) + " bid: "
+                      + r['bids'][0][0] + " ask: " + r['asks'][0][0])
+
                 new_nbbo_size = get_nbbo_size(new_signal)
                 if (new_nbbo_size < (nbbo_size/3) and new_nbbo_size < max_size) or (new_nbbo_size < small_size):
-                    trade = execute_trade(new_signal, trade_size, product_id)
+                    trade = execute_trade(signal, trade_size, product_id)
                     if trade is not None:
                         traded = True
                         time.sleep(1)
                         order = requests.get(api_url + 'orders/' + trade['id'], auth=auth).json()
                         filled_quantity = float(order['filled_size'])
                         print("            position size: " + str(filled_quantity))
-                        price = float(order['price'])
+                        entry = float(order['price'])
                         cancel = requests.delete(api_url + 'orders/', auth=auth).json()
                         print("      canceled: ")
                         print(cancel)
-                    time.sleep(1)
 
                 r = requests.get(api_url + 'products/' + "LTC-USD" + "/book").json()
                 new_signal = order_pressure(ratio, depth)
-                print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " signal: " + str(new_signal) + " bid: "
-                      + r['bids'][0][0] + " ask: " + r['asks'][0][0])
-
                 time.sleep(1)
 
             while filled_quantity > 0.01:
-                print("           Exiting Position....")
-                if exit_signal(signal, price, profit, loss):
+                if exit_trade is None:
+                    print("        Exiting Position...")
+                    exit_trade = execute_exit_trade(signal, filled_quantity, product_id, entry, gain)
+                if exit_trade is not None:
+                    time.sleep(1)
+                    order = requests.get(api_url + 'orders/' + exit_trade['id'], auth=auth).json()
+                    filled_quantity = filled_quantity - float(order['filled_size'])
+
+                if filled_quantity < .01:
+                    print("       Locked in Profit! Woo")
+                    print("      " + order['side'] + " " + order['size'] + " " + order['product_id'] + " at " + str(order['price']))
+                    print(order)
+
+                if stop_loss(signal, entry, loss) and filled_quantity > .01:
+                    print("       Stop Loss Triggered")
                     trade = execute_trade(-signal, filled_quantity, product_id)
                     if trade is not None:
                         time.sleep(1)
                         order = requests.get(api_url + 'orders/' + trade['id'], auth=auth).json()
                         filled_quantity = filled_quantity - float(order['filled_size'])
                         print("            position size: " + str(filled_quantity))
-                        price = float(order['price'])
                         cancel = requests.delete(api_url + 'orders/', auth=auth).json()
                         print("      canceled: ")
                         print(cancel)
-                time.sleep(1)
+
+            if traded:
+                cancel = requests.delete(api_url + 'orders/', auth=auth).json()
+                print("      End of trade cycle: cancel all")
+                print(cancel)
 
         time.sleep(1)
 
 
 if __name__ == "__main__":
-    ratio = 4
+    ratio = 5
     depth = 5
-    max_size = 5
+    max_size = 4
     small_size = 1
     trade_size = .1
     product_id = 'LTC-USD'
-    profit = .06
+    gain = .08
     loss = .03
     api_url = 'https://api.gdax.com/'
     auth = CoinbaseExchangeAuth()
