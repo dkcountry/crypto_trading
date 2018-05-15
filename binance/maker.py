ff24705c5da6f60a72d35a5f750a29e41b52fe5a
David Kang <dkang009@gmail.com>
2018-05-14 22:24:12 -0500

    init commit

---
+++ binance/maker.py
@@ -0,0 +1,47 @@
+from binance.client import Client
+import util.time_converter as tc
+import pandas as pd
+import numpy as np
+pd.options.display.float_format = '{%.2f}'.format
+
+
+class MarketMaker(object):
+    def __init__(self, api_key, api_secret, ticker, num_levels):
+        self.client = Client(api_key, api_secret)
+        self.ticker = ticker
+        self.num_levels = num_levels
+        self.position = 0
+        self.avg_price = np.nan
+        self.fees = 0
+        self.revenue = 0
+        self.open_orders = pd.DataFrame()
+
+    def fetch_all_orders(self):
+        """Get all open orders and save to df"""
+
+        orders = self.client.get_open_orders(symbol=self.ticker)
+        columns = ['symbol', 'orderId', 'price', 'origQty', 'executedQty', 'status', 'timeInForce',
+                   'type', 'side', 'isWorking']
+        o = pd.DataFrame([[x[col] for col in columns] for x in orders], columns=columns)
+        self.open_orders = o
+
+    def set_mid_price(self):
+        order_book = self.client.get_order_book(symbol=self.ticker)
+        best_bid = float(order_book['bids'][0][0])
+        best_ask = float(order_book['asks'][0][0])
+        mid = round(np.average([best_bid, best_ask]), 6)
+        return mid
+
+    def cancel_one_side(self, side):
+        ids = self.open_orders[self.open_orders.side == side.upper()].orderId.values
+        for orderId in ids:
+            result = self.client.cancel_order(symbol=self.ticker, orderId=orderId)
+        self.fetch_all_orders()
+
+
+m = MarketMaker(api_key, api_secret, "NEOBTC", 3)
+
+if __name__ == "__main__":
+
+    m = MarketMaker(api_key, api_secret, "NEOBTC", 3)
+    print(m.set_mid_price())
