1a7e9f6e4866a14ba7cd2df0454897f078234a6c
David Kang <dkang009@gmail.com>
2018-05-19 15:51:09 -0500

    updates dynamic market adjustments

--- binance/maker.py
+++ binance/maker.py
@@ -21,15 +21,25 @@ class MarketMaker(object):
         self.open_orders = pd.DataFrame()
 
     def fetch_all_orders(self):
-        """Get all open orders and save to df"""
+        """Get all open orders and save to object df"""
 
         orders = self.client.get_open_orders(symbol=self.ticker)
         columns = ['symbol', 'orderId', 'price', 'origQty', 'executedQty', 'status', 'timeInForce',
                    'type', 'side', 'isWorking']
         o = pd.DataFrame([[x[col] for col in columns] for x in orders], columns=columns)
         self.open_orders = o
+        self.format_open_orders()
+
+    def format_open_orders(self):
+        """convert price and quantity columns in open orders to float"""
+
+        self.open_orders.price = pd.to_numeric(self.open_orders.price)
+        self.open_orders.origQty = pd.to_numeric(self.open_orders.origQty)
+        self.open_orders.executedQty = pd.to_numeric(self.open_orders.executedQty)
 
     def set_mid_price(self):
+        """set mid point price """
+
         order_book = self.client.get_order_book(symbol=self.ticker)
         best_bid = float(order_book['bids'][0][0])
         best_ask = float(order_book['asks'][0][0])
@@ -47,44 +57,55 @@ class MarketMaker(object):
         if side.upper() == 'BUY':
             best = mid - self.edge
             for i in range(self.num_levels):
-                o = self.client.order_limit_buy(symbol=self.ticker, quantity=self.qty,
-                                                price=round(best-self.edge*i, 6))
+                o = self.client.order_limit_buy(symbol=self.ticker, quantity=self.qty, price=round(best-self.edge*i, 6))
 
         if side.upper() == 'SELL':
             best = mid + self.edge
             for i in range(self.num_levels):
-                o = self.client.order_limit_sell(symbol=self.ticker, quantity=self.qty,
-                                                 price=round(best+self.edge*i, 6))
+                o = self.client.order_limit_sell(symbol=self.ticker, quantity=self.qty, price=round(best+self.edge*i,6))
 
     def cancel_all(self):
+        """cancel all orders"""
+
         self.cancel_one_side("BUY")
         self.cancel_one_side("SELL")
 
     def update_position(self):
+        """Iterate through open orders; if executed quantity has changed, updated self.position and self.avg_price
+            then update open orders
+        """
         position_changed = False
         old_orders = self.open_orders.copy(deep=True)
+
         for ind, row in old_orders.iterrows():
             order = self.client.get_order(symbol=row['symbol'], orderId=row['orderId'])
             qty_diff = float(order['executedQty']) - float(row['executedQty'])
+
             if qty_diff > .00001:
                 print(qty_diff)
                 self.record_fill(qty_diff, float(order['price']), order['side'])
                 position_changed = True
                 self.open_orders = self.open_orders[self.open_orders.orderId != order['orderId']]
+
                 columns = ['symbol', 'orderId', 'price', 'origQty', 'executedQty', 'status', 'timeInForce',
                            'type', 'side', 'isWorking']
                 if order['status'] not in ['FILLED', 'CANCELED']:
-                    mod_o = [order[col] for col in columns]
-                    mod_o = pd.Series(mod_o, index=columns)
-                    self.open_orders = self.open_orders.append(mod_o)
+                    mod_order = [order[col] for col in columns]
+                    mod_order = pd.Series(mod_order, index=columns)
+                    self.open_orders = self.open_orders.append(mod_order)
+                    self.format_open_orders()
                     print(self.open_orders)
 
+            if order['status'] in ['CANCELED']:
+                self.open_orders = self.open_orders[self.open_orders.orderId != order['orderId']]
 
         print("{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()) + " " +
               "current position: " + str(self.position) + " avg fill price: " + str(self.avg_price))
         return position_changed
 
     def record_fill(self, qty_diff, price, side):
+        """handles updating self.position and self.avg_price"""
+
         if side.upper() == 'BUY' and self.position >= 0:
             self.avg_price = (self.avg_price * self.position + qty_diff * price)/(qty_diff + self.position)
             self.position += qty_diff
@@ -101,25 +122,46 @@ class MarketMaker(object):
                 self.avg_price = price
 
     def adjust_orders(self):
-        if pd.to_numeric(self.open_orders[self.open_orders.side == 'BUY'].price).max() < (self.avg_price - 2.5*self.edge) \
-                and self.position <= self.max_position:
-            sm_prc = self.open_orders[self.open_orders.side == 'BUY'].price.min()
-            order = self.open_orders[(self.open_orders.side == 'BUY') & (self.open_orders.price == sm_prc)].iloc[0]
+        """dynamically adjust orders"""
+
+        # if a position change results in flat position, reset markets
+        if self.position == 0:
+            self.cancel_all()
+            return
+
+        # handle buy orders
+        least_agro_buy = self.open_orders[self.open_orders.side == 'BUY'].price.min()
+        if least_agro_buy < (self.avg_price - 2.5*self.edge) and self.position <= self.max_position:
+            order = self.open_orders[(self.open_orders.price == least_agro_buy)].iloc[0]
             cancel = self.client.cancel_order(symbol=self.ticker, orderId=order.orderId)
             o = self.client.order_limit_buy(symbol=self.ticker, quantity=self.qty,
                                             price=round(self.avg_price - 2*self.edge, 6))
             print(o)
 
-        if pd.to_numeric(self.open_orders[self.open_orders.side == 'SELL'].price).min() > (self.avg_price + 2.5*self.edge) \
-                and -self.position <= self.max_position:
-            lg_prc = self.open_orders[self.open_orders.side == 'SELL'].price.max()
-            order = self.open_orders[(self.open_orders.side == 'SELL') & (self.open_orders.price == lg_prc)].iloc[0]
+        if pd.isna(least_agro_buy) and self.position <= self.max_position:
+            o = self.client.order_limit_buy(symbol=self.ticker, quantity=self.qty,
+                                            price=round(self.avg_price - 2 * self.edge, 6))
+            print(o)
+
+        # handle sell orders
+        least_agro_sell = self.open_orders[self.open_orders.side == 'SELL'].price.max()
+        if least_agro_sell > (self.avg_price + 2.5*self.edge) and -self.position <= self.max_position:
+            order = self.open_orders[(self.open_orders.price == least_agro_sell)].iloc[0]
             cancel = self.client.cancel_order(symbol=self.ticker, orderId=order.orderId)
             o = self.client.order_limit_sell(symbol=self.ticker, quantity=self.qty,
                                              price=round(self.avg_price + 2*self.edge, 6))
             print(o)
 
+        if pd.isna(least_agro_sell) and -self.position <= self.max_position:
+            o = self.client.order_limit_sell(symbol=self.ticker, quantity=self.qty,
+                                            price=round(self.avg_price + 2 * self.edge, 6))
+            print(o)
+
+        self.fetch_all_orders()
+
     def set_init_market(self):
+        """initialize orders"""
+
         self.cancel_all()
         init_mid = self.set_mid_price()
         self.place_one_side("BUY", init_mid)
@@ -127,14 +169,15 @@ class MarketMaker(object):
         self.fetch_all_orders()
 
     def run(self):
-        self.set_init_market()
+        """run algo """
 
+        self.set_init_market()
         while True:
             change = self.update_position()
             if change:
                 self.adjust_orders()
 
-            # if no open orders, restart script
+            # if no open orders, reset markets
             if len(self.open_orders) == 0:
                 self.set_init_market()
 
@@ -142,7 +185,5 @@ class MarketMaker(object):
 
 
 if __name__ == "__main__":
-
-
     m = MarketMaker(api_key, api_secret, "NEOBTC", 2, .16, 0.000005)
     m.run()
