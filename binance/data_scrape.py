b5376910f86511633565aad483f81568d2f8dfbf
David Kang <dkang009@gmail.com>
2018-05-19 15:59:59 -0500

    adds read data functionality

--- binance/data_scrape.py
+++ binance/data_scrape.py
@@ -2,8 +2,8 @@ from binance.client import Client
 import time
 import datetime
 import csv
+import pandas as pd
 import util.time_converter as tc
-
 
 client = Client(api_key, api_secret)
 
@@ -16,17 +16,28 @@ info = client.get_symbol_info("LTCBTC")
 
 
 def collect_order_data(depth):
-    order_book = client.get_order_book(symbol='LTCBTC')
-    row = [round(time.time())]
-    for i in range(depth):
-        row.append(order_book['bids'][0][0])
-        row.append(order_book['bids'][0][1])
-        row.append(order_book['asks'][0][0])
-        row.append(order_book['asks'][0][1])
+    while True:
+        order_book = client.get_order_book(symbol='LTCBTC')
+        row = [round(time.time())]
+        for i in range(depth):
+            row.append(order_book['bids'][0][0])
+            row.append(order_book['bids'][0][1])
+            row.append(order_book['asks'][0][0])
+            row.append(order_book['asks'][0][1])
 
-    with open('order_book.csv', 'a') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(row)
+        with open('binance/order_book.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(row)
 
-    print("wrote row at: " + str(round(time.time())))
-    time.sleep(1)
+        print("wrote row at: " + str(round(time.time())))
+        time.sleep(1)
+
+
+def read_data():
+    """read from csv"""
+
+    with open('binance/order_book.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        data = [x for x in reader]
+    df = pd.DataFrame(data)
+    return df.apply(pd.to_numeric)
